b17f6f8281fbde16cc92d8b759e063362fb194ac
Taci Ugraskan <143652012+tugraskan@users.noreply.github.com>
2025-06-13 16:32:46 -0500

    Enhance fertilizer data reading logic
    Modified the code to prioritize reading from `fertilizer_ext.frt`. If this file is absent, it checks `in_parmdb%fert_frt`. Allocates `fertdb` based on the data read, ensuring compatibility with existing code by using `fertdb_cbn` when available. Removed obsolete lines to streamline the workflow.

--- src/fert_parm_read.f90
+++ src/fert_parm_read.f90
@@ -19,40 +19,82 @@
       imax = 0
       mfrt = 0
       
-      inquire (file=in_parmdb%fert_frt, exist=i_exist)
-      if (.not. i_exist .or. in_parmdb%fert_frt == "null") then
-         allocate (fertdb(0:0))
-      else
-      do  
-        open (107,file=in_parmdb%fert_frt)
-        read (107,*,iostat=eof) titldum
-        if (eof < 0) exit
-        read (107,*,iostat=eof) header
-        if (eof < 0) exit
-           do while (eof == 0) 
-             read (107,*,iostat=eof) titldum
-             if (eof < 0) exit
-             imax = imax + 1
-           end do
+      
+      !! --- inquire if fertilizer_ext.frt exists, if not proceed with old input read --- !!
+      inquire (file='fertilizer_ext.frt', exist=i_exist)
+      if (.not. i_exist) then
+          allocate (fertdb_cbn(0:0)) 
+          
+          ! if fertilizer_ext.frt does not exist, check for in_parmdb%fert_frt, use old workflow
+          inquire (file=in_parmdb%fert_frt, exist=i_exist)
+          if (.not. i_exist .or. in_parmdb%fert_frt == "null") then
+             allocate (fertdb(0:0))
+          else
+              do  
+                open (107,file=in_parmdb%fert_frt)
+                read (107,*,iostat=eof) titldum
+                if (eof < 0) exit
+                read (107,*,iostat=eof) header
+                if (eof < 0) exit
+                   do while (eof == 0) 
+                     read (107,*,iostat=eof) titldum
+                     if (eof < 0) exit
+                     imax = imax + 1
+                   end do
            
-        allocate (fertdb(0:imax))
+                allocate (fertdb(0:imax))
         
-        rewind (107)
-        read (107,*,iostat=eof) titldum
-        if (eof < 0) exit
-        read (107,*,iostat=eof) header
-        if (eof < 0) exit
+                rewind (107)
+                read (107,*,iostat=eof) titldum
+                if (eof < 0) exit
+                read (107,*,iostat=eof) header
+                if (eof < 0) exit
         
-        do it = 1, imax
-          read (107,*,iostat=eof) fertdb(it)
-          if (eof < 0) exit
-        end do
-       exit
-      enddo
+                do it = 1, imax
+                  read (107,*,iostat=eof) fertdb(it)
+                  if (eof < 0) exit
+                end do
+               exit
+              enddo
+          endif
+      
+      else
+          !! --- if fertilizer_ext.frt exists,read it instead and fertdb = fertdb_cbn%base  --- !!
+          do  
+            open (107,file='fertilizer_ext.frt')
+            read (107,*,iostat=eof) titldum
+            if (eof < 0) exit
+            read (107,*,iostat=eof) header
+            if (eof < 0) exit
+               do while (eof == 0) 
+                 read (107,*,iostat=eof) titldum
+                 if (eof < 0) exit
+                 imax = imax + 1
+               end do
+           
+            allocate (fertdb(0:imax))
+            allocate (fertdb_cbn(0:imax))
+        
+            rewind (107)
+            read (107,*,iostat=eof) titldum
+            if (eof < 0) exit
+            read (107,*,iostat=eof) header
+            if (eof < 0) exit
+        
+            do it = 1, imax
+              read (107,*,iostat=eof) fertdb_cbn(it)
+              if (eof < 0) exit
+              !-- Assign fertdb_cbn to fertdb for compatibility with existing code --- !
+              fertdb(it) = fertdb_cbn(it)%base
+            end do
+           exit
+          enddo
       endif
       
       db_mx%fertparm  = imax 
       
       close (107)
+      
+            
       return
       end subroutine fert_parm_read
