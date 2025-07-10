e077e2a7fec2c7a0c401a83a10a7f91da720470d
Taci Ugraskan <143652012+tugraskan@users.noreply.github.com>
2025-07-10 13:12:06 -0500

    Update manure name composition and crosswalk

--- src/manure_parm_read.f90
+++ src/manure_parm_read.f90
@@ -8,8 +8,8 @@
    
       integer :: it = 0                     !!none      |counter
       character (len=80) :: titldum = ""    !!          |title of file
-      character (len=80) :: header = ""     !!          |header of file
-      character (len=16) :: omad_line = ""  !!          |omad line dummy variable
+      character (len=256) :: header = ""     !!          |header of file
+      character (len=256) :: csv_line = ""  !! line for csv file
       integer :: eof = 0                    !!          |end of file
       integer :: unit = 107                 !!          |unit number for file
       integer :: imax = 0                   !!none      |determine max number for array (imax) and total number in file
@@ -61,50 +61,61 @@
       eof = 0
       imax = 0
       
-      !! --- inquire if omad.txt exists --- !!
-      inquire (file="omad.txt", exist=i_exist)
-      if (.not. i_exist .or. "omad.txt" == "null") then
+      !! --- inquire if fp5-manure-content-defaults-swat.csv exists --- !!
+      inquire (file="fp5-manure-content-defaults-swat.csv", exist=i_exist)
+      if (.not. i_exist .or. "fp5-manure-content-defaults-swat.csv" == "null") then
           allocate (omad_db(0:0))
       else
-            open (unit,file="omad.txt")
+            open (unit,file="fp5-manure-content-defaults-swat.csv")
+            read(unit,'(A)',iostat=eof) header   ! skip header line
             do while (eof == 0)
-                read (unit,*,iostat=eof) omad_line ! read dummy omad line
+                read(unit,'(A)',iostat=eof) csv_line
                 if (eof < 0) exit
                 imax = imax + 1
             end do
-            !! ensure that imax is a multiple of 8 and omad is in proper format 
-            if (mod(imax, 8) /= 0) then
-                ! if not then write error message to diagnostics.out file and allocate empty omad_db
-                write (9001, *) "Error: OMAD file does not have a multiple of 8 lines / not in proper format"
-                allocate (omad_db(0:0))       
-            else
-                imax = imax / 8
-                allocate (omad_db(0:imax))
-                rewind (unit)
-                !! read the omad database
-                do it = 1, imax
-                    read(unit, *, iostat=eof) omad_db(it)%manure_name, omad_db(it)%manure_desc
-                    read(unit, *, iostat=eof) omad_db(it)%omadtyp
-                    read(unit, *, iostat=eof) omad_db(it)%astgc
-                    read(unit, *, iostat=eof) omad_db(it)%astlbl
-                    read(unit, *, iostat=eof) omad_db(it)%astlig
-                    read(unit, *, iostat=eof) omad_db(it)%cn
-                    read(unit, *, iostat=eof) omad_db(it)%cp
-                    read(unit, *, iostat=eof) omad_db(it)%cs
-                end do
-            endif
-            close (unit)
-            
-            !xwalk fertdb_cbn(it)%omad%manure_name to omad_db(it)%manure_name
-
-            do i = 1, size(fertdb_cbn)
-              do it = 1, size(omad_db)
-                if (fertdb_cbn(i)%omad%manure_name == omad_db(it)%manure_name) then
-                  fertdb_cbn(i)%omad = omad_db(it)
-                  exit  ! Stop searching once a match is found
-                end if
-              end do
+            allocate (omad_db(0:imax))
+            rewind(unit)
+            read(unit,'(A)',iostat=eof) header
+            do it = 1, imax
+                read(unit,'(A)',iostat=eof) csv_line
+                if (eof < 0) exit
+                read(csv_line,*) &
+                     omad_db(it)%manure_region, &
+                     omad_db(it)%manure_source, &
+                     omad_db(it)%manure_type, &
+                     omad_db(it)%pct_moisture, &
+                     omad_db(it)%pct_solids, &
+                     omad_db(it)%total_c, &
+                     omad_db(it)%total_n, &
+                     omad_db(it)%inorganic_n, &
+                     omad_db(it)%organic_n, &
+                     omad_db(it)%total_p2o5, &
+                     omad_db(it)%inorganic_p2o5, &
+                     omad_db(it)%organic_p2o5, &
+                     omad_db(it)%inorganic_p, &
+                     omad_db(it)%organic_p, &
+                     omad_db(it)%solids, &
+                     omad_db(it)%water, &
+                     omad_db(it)%units, &
+                     omad_db(it)%sample_size, &
+                     omad_db(it)%summary_level, &
+                     omad_db(it)%data_source
+                omad_db(it)%manure_name = trim(omad_db(it)%manure_region)//trim(omad_db(it)%manure_source)//"_"// &
+                     trim(omad_db(it)%manure_type)
             end do
+            close (unit)
+
+            ! crosswalk OMAD records to entries loaded from fertilizer_ext.frt
+            if (size(fertdb_cbn) > 0 .and. size(omad_db) > 0) then
+              do i = 1, size(fertdb_cbn)
+                do it = 1, size(omad_db)
+                  if (trim(fertdb_cbn(i)%omad%manure_name) == trim(omad_db(it)%manure_name)) then
+                    fertdb_cbn(i)%omad = omad_db(it)
+                    exit  ! Stop searching once a match is found
+                  end if
+                end do
+              end do
+            end if
         endif
              
       return
