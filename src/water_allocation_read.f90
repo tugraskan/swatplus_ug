3b017eed04660a0fc981d100988874a90301ab84
tugraskan <12345678+tugraskan@users.noreply.github.com>
2025-09-02 15:30:13 -0500

    wro

--- src/water_allocation_read.f90
+++ src/water_allocation_read.f90
@@ -94,7 +94,8 @@
             backspace (107)
             read (107,*,iostat=eof) k, wallo(iwro)%dmd(i)%ob_typ, wallo(iwro)%dmd(i)%ob_num,            &
               wallo(iwro)%dmd(i)%dmd_typ, wallo(iwro)%dmd(i)%dmd_typ_name, wallo(iwro)%dmd(i)%amount,   &
-              wallo(iwro)%dmd(i)%right, wallo(iwro)%dmd(i)%src_num, wallo(iwro)%dmd(i)%rcv_num
+              wallo(iwro)%dmd(i)%right, wallo(iwro)%dmd(i)%rcv_num, wallo(iwro)%dmd(i)%src_num !, &
+              !wallo(iwro)%dmd(i)%dtl_rcv_fr, wallo(iwro)%dmd(i)%dtl_src_fr
             num_src = wallo(iwro)%dmd(i)%src_num
             allocate (wallo(iwro)%dmd(i)%src(num_src))
             allocate (wallod_out(iwro)%dmd(i)%src(num_src))
@@ -147,21 +148,22 @@
             backspace (107)
             read (107,*,iostat=eof) k, wallo(iwro)%dmd(i)%ob_typ, wallo(iwro)%dmd(i)%ob_num,            &
               wallo(iwro)%dmd(i)%dmd_typ, wallo(iwro)%dmd(i)%dmd_typ_name, wallo(iwro)%dmd(i)%amount,   &
-              wallo(iwro)%dmd(i)%right, wallo(iwro)%dmd(i)%src_num, wallo(iwro)%dmd(i)%rcv_num,         &
-              (wallo(iwro)%dmd(i)%src(isrc), isrc = 1, num_src),                                        &
-              (wallo(iwro)%dmd(i)%rcv(ircv), ircv = 1, num_rcv)
+              wallo(iwro)%dmd(i)%right, wallo(iwro)%dmd(i)%rcv_num, wallo(iwro)%dmd(i)%src_num,         &
+              !wallo(iwro)%dmd(i)%dtl_rcv_fr, wallo(iwro)%dmd(i)%dtl_src_fr, &
+              (wallo(iwro)%dmd(i)%src(ircv), ircv = 1, num_rcv),                                        &
+              (wallo(iwro)%dmd(i)%rcv(isrc), isrc = 1, num_src)
             
             !! set src_wal links to main source objects
-            do isrc = 1, num_src
+            !do isrc = 1, num_src
               !! find the corresponding source object in the main source list
-              do jsrc = 1, wallo(iwro)%src_obs
-                if (wallo(iwro)%dmd(i)%src(isrc)%src_typ == wallo(iwro)%src(jsrc)%ob_typ .and. &
-                    wallo(iwro)%dmd(i)%src(isrc)%src_num == wallo(iwro)%src(jsrc)%ob_num) then
-                  wallo(iwro)%dmd(i)%src(isrc)%src_wal = jsrc
-                  exit
-                end if
-              end do
-            end do
+              !do jsrc = 1, wallo(iwro)%src_obs
+                !if (wallo(iwro)%dmd(i)%src(isrc)%src_typ == wallo(iwro)%src(jsrc)%ob_typ .and. &
+                    !wallo(iwro)%dmd(i)%src(isrc)%src_num == wallo(iwro)%src(jsrc)%ob_num) then
+                  !wallo(iwro)%dmd(i)%src(isrc)%src_wal = jsrc
+                  !exit
+                !end if
+              !end do
+            !end do
             
             !! zero output variables for summing
             do isrc = 1, num_src
